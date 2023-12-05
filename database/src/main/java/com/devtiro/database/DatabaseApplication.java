4ef8c9ccbfb09aad0f284654db0cdd8f098f51cc
Sriharyi <sriharyi.c@gmail.com>
2023-12-05 10:52:22 +0530

    h2 in memory database connection

---
+++ database/src/main/java/com/devtiro/database/DatabaseApplication.java
@@ -0,0 +1,32 @@
+package com.devtiro.database;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import lombok.extern.java.Log;
+
+@SpringBootApplication
+@Log
+public class DatabaseApplication implements CommandLineRunner {
+
+	private DataSource dataSource;
+
+	public DatabaseApplication(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("DataSource: " + dataSource.toString());
+		final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.execute("select 1");	
+	}
+
+}
