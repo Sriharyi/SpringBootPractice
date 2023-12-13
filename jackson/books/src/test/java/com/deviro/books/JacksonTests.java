f65304d3f2f9c03b9ff8a01af10097bac4707415
Sriharyi <sriharyi.c@gmail.com>
2023-12-13 15:09:10 +0530

    json to java unmarshalling process

--- jackson/books/src/test/java/com/deviro/books/JacksonTests.java
+++ jackson/books/src/test/java/com/deviro/books/JacksonTests.java
@@ -2,6 +2,7 @@ package com.deviro.books;
 
 import com.deviro.books.domain.Book;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -26,5 +27,26 @@ public class JacksonTests {
         .isEqualTo(
             "{\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of"
                 + " Eternity\",\"author\":\"Aria Montgomery\",\"yearPublished\":\"2005\"}");
+  }
+
+  @Test
+  public void testthatCreateObjectMapperCanCreateJavaObjectFromJson()
+      throws JsonMappingException, JsonProcessingException {
+    String json =
+        "{\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of"
+            + " Eternity\",\"author\":\"Aria Montgomery\",\"yearPublished\":\"2005\"}";
+    Book book =
+        Book.builder()
+            .isbn("978-0-13-478627-5")
+            .title("The Enigma of Eternity")
+            .author("Aria Montgomery")
+            .yearPublished("2005")
+            .build();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    Book resultBook = objectMapper.readValue(json, Book.class);
+
+    Assertions.assertThat(resultBook).isEqualTo(book);
   }
 }
