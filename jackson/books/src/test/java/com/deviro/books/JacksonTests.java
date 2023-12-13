147015ef37c7110475ed711213c43118fe1d98c5
Sriharyi <sriharyi.c@gmail.com>
2023-12-13 15:35:52 +0530

    JsonIgnoreProperties(ignoreUnknown = true} for UnrecognisedpropertyException

--- jackson/books/src/test/java/com/deviro/books/JacksonTests.java
+++ jackson/books/src/test/java/com/deviro/books/JacksonTests.java
@@ -33,7 +33,7 @@ public class JacksonTests {
   public void testthatCreateObjectMapperCanCreateJavaObjectFromJson()
       throws JsonMappingException, JsonProcessingException {
     String json =
-        "{\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of"
+        "{\"foo\":\"boo\",\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of"
             + " Eternity\",\"author\":\"Aria Montgomery\",\"year\":\"2005\"}";
     Book book =
         Book.builder()
